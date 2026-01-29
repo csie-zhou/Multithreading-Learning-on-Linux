6b2044c2108848917df202ccfb2f74f7dd119485
Chou <148289135+csie-zhou@users.noreply.github.com>
2026-01-29 15:30:54 +0800

    Update thread_pool.c

--- Chapter 2/src/thread_pool.c
+++ Chapter 2/src/thread_pool.c
@@ -2,6 +2,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int thread_pool_add(thread_pool_t *pool, void (*function)(void *), void *argument) {
+    if (pool == NULL || function == NULL) {
+        return -1; // Invalid arguments
+    }
+
+    /* 1. Lock for Queue */
+    if (pthread_mutex_lock(&(pool->lock)) != 0) {
+        return -1;
+    }
+
+    /* 2. Check if Queue is full */
+    if (pool->count == pool->queue_size) {
+        // Queue Full
+        pthread_mutex_unlock(&(pool->lock));
+        return -2; // -2 represents full (Common seen in Driver：-ENOMEM or -EBUSY)
+    }
+
+    /* 3. Add task to Tail */
+    pool->queue[pool->tail].function = function;
+    pool->queue[pool->tail].argument = argument;
+
+    /* 4. Update Tail (Circular Buffer) */
+    pool->tail = (pool->tail + 1) % pool->queue_size;
+    pool->count++;
+
+    /* 5. Unlock */
+    pthread_mutex_unlock(&(pool->lock));
+
+    /* TODO: Chapter 6. Call pthread_cond_signal to announce Worker new task */
+
+    return 0; // Success
+}
+
 /* Testing function for Chapter 2, manually consume task for Worker Thread */
 int thread_pool_run_pending_task(thread_pool_t *pool) {
     thread_task_t task;
